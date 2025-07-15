54a211aba6f87633d3d89ec3ee6d4d4c2f024f0b
tranquillo_manvith <manvithpoojary095@gmail.com>
2025-07-15 22:17:06 +0530

    navbar designed

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { JSX } from "react";
+import Link from "next/link";
+
+export default function Navbar(): JSX.Element {
+  return (
+    <>
+      <div className="flex justify-center items-center text-2xl py-[32px]">
+        Project Small - 3: Currency Converter
+      </div>
+      <div>
+        <ul>
+          <li className="flex gap-[32px] px-[64px]">
+            <Link className="hover:text-[#858585]" href={"/"}>
+              Home
+            </Link>
+            <Link className="hover:text-[#858585]" href={"/other_projects"}>
+              Other Projects
+            </Link>
+            <Link className="hover:text-[#858585]" href={"/details"}>
+              Details
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
